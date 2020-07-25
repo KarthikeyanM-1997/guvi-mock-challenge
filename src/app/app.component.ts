7e7c751758871a8fc87cb5ddd0348140e4550d9a
Vetox <karthikeyan1997@gmail.com>
2020-07-25 15:21:53 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Trial1';
+  email = new FormControl("");
+  password = new FormControl("");
+  
+  signIn(){
+    console.log("Email : " + this.email.value);
+    console.log("Password : " + this.password.value);
+  }
 }
